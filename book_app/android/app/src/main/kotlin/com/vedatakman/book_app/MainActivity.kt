3f6966eac53180fa2096e02e916eb19336a8bf1e
22asli <celik.aslihan.97@gmail.com>
2024-09-24 14:44:54 +0300

    yeni nesil okuyor
    ilk okul çocukları için kitap okuma uygulaması

---
+++ book_app/android/app/src/main/kotlin/com/vedatakman/book_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.vedatakman.book_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ book_app/android/app/src/main/kotlin/com/vedatakman/bookapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.vedatakman.bookapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
